00bda14feb918e004f4d6343ca0bbd72d19020dc
Nurjahan Jhorna <nj32@njit.edu>
2023-12-15 19:52:43 -0500

    Added to HomeNavbar.js and index.js, created HomeNavbar.module.css, component.js, and _app.js

---
+++ my-shop/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      {/* rest of your page content */}
+    </div>
+  );
+};
+
+export default Home;
