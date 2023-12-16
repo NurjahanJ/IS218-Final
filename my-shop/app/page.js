59c9322b7fc9d900effec2939da5459bff871b02
Nurjahan Jhorna <nj32@njit.edu>
2023-12-16 17:00:12 -0500

    coded MainBannner.js component and added it to the page.js file fixed the image issue in the header file

--- my-shop/app/page.js
+++ my-shop/app/page.js
@@ -1,9 +1,11 @@
 import Header from '@/component/Header'
 import Review from '@/component/Review'
+import MainBanner from '@/component/MainBanner'
 export default function Page() {
   return (
     <div>
       <Header />
+      <MainBanner />
       <Review />
     </div>
   )
