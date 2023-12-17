9e7d277b49f5286fb675475fbb7db303ab110ab0
Nurjahan Jhorna <nj32@njit.edu>
2023-12-16 20:21:14 -0500

    added new branch and made new component

--- my-shop/app/page.js
+++ my-shop/app/page.js
@@ -1,7 +1,9 @@
+'use client'
 import Header from '@/component/Header'
 import Review from '@/component/Review'
 import MainBanner from '@/component/MainBanner'
 import Subscription from '@/component/Subscription'
+import Footer from '@/component/Footer'
 
 export default function Page() {
   return (
@@ -10,6 +12,7 @@ export default function Page() {
       <MainBanner />
       <Subscription />
       <Review />
+      <Footer />
     </div>
   )
 }
