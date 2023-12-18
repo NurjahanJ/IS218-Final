6f96dcf5eb75bd0e623a7f9021b0a40a4c723e2e
Nurjahan Jhorna <nj32@njit.edu>
2023-12-18 15:44:24 -0500

    updated header-navbar

--- my-shop/component/Header.js
+++ my-shop/component/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
-import { Popover, PopoverTrigger, PopoverContent, Button } from "@nextui-org/react";
-import '../styles/header.css'; 
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
+import '../styles/header.css';
 
 export default function Header() {
   return (
     <Navbar className="navbar-bg">
       <NavbarBrand className="logo">
         <img src="/images/Logo.jpg" alt="Wander Glow Logo" width="67"/>
-        <NavbarContent className="nav-content">
+      </NavbarBrand>
+      <NavbarContent className="nav-content">
         <NavbarItem>
           <Link href="./" className="nav-link">
             Home
@@ -19,27 +20,22 @@ export default function Header() {
             Candles
           </Link>
         </NavbarItem>
-
         <NavbarItem isActive>
-            <Popover placement="bottom" showArrow={true}>
-              <PopoverTrigger>
-                <Link href="#" className="nav-link">
-                  Cart
-                </Link>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="px-1 py-2">
-                  <div className="text-small font-bold">Cart</div>
-                  <div className="text-tiny">Your cart is currently empty.</div>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </NavbarItem>
-
-        </NavbarContent>
-
-      </NavbarBrand>
-
+          <Popover placement="bottom" showArrow={true}>
+            <PopoverTrigger>
+              <Link href="#" className="nav-link">
+                Cart
+              </Link>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2">
+                <div className="text-small font-bold">Cart</div>
+                <div className="text-tiny">Your cart is currently empty.</div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </NavbarItem>
+      </NavbarContent>
     </Navbar>
   );
 }
