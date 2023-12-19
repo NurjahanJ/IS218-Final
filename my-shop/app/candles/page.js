5f405042a0f7083e0717914da266910e7e0b91b8
“Mili <mk237@njit.edu>
2023-12-18 20:06:37 -0500

    Adding Google Analytics to other Websites

--- my-shop/app/candles/page.js
+++ my-shop/app/candles/page.js
@@ -1,8 +1,24 @@
 'use client'
+import Head from 'next/head';
 import Candles from "@/component/candles"
 export default function Page() {
   return (
     <div>
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8F7WJ3R8GN"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8F7WJ3R8GN');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
+      </Head>
       <Candles />
     </div>
   )
